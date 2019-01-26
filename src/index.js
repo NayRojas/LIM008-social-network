f08eda8a2ef1f52df65d186237b183583587e680
NayRojas <nayllen.rojas@gmail.com>
2019-01-25 22:09:32 -0500

    #16 Agregar mensaje de error para email y contraseña invalida

--- src/index.js
+++ src/index.js
@@ -1,38 +1,95 @@
 // Este es el punto de entrada de tu aplicacion
+
+// Estado inicial de pantallas
 document.getElementById('first-screen').style.display = 'block';
 document.getElementById('second-screen').style.display = 'none';
 
+// Constantes para manipulación del DOM
 const btnSingUp = document.getElementById('button-to-sign-up');
 const btnCreateUser = document.getElementById('button-to-create-new-user');
-const nameTxt = document.getElementById('name');
+const btnSignUpWithGoogle = document.getElementById('button-to-create-new-user-google');
+// const nameTxt = document.getElementById('name');
 const emailTxt = document.getElementById('email');
 const passwordTxt = document.getElementById('password');
+const welcome = document.getElementById('welcome-new-user-screen');
+const errorWeakPassword = document.getElementById('error-message-password');
+const errorInvalidEmail = document.getElementById('error-messase-invalid-email');
 
-//Añadir evento de cambio de vista a crear nuevo usuario
+// Añadir evento de cambio de vista a crear nuevo usuario
 btnSingUp. addEventListener('click', () => {
-    document.getElementById('first-screen').style.display = 'none';
-    document.getElementById('second-screen').style.display = 'block';
+  showSecondScreen();
 });
 
-//Añadir evento SignUp
+// Añadir evento Creat user con correo electronico y contraseña
 btnCreateUser.addEventListener('click', e => {
-    e.preventDefault();
-    document.getElementById('second-screen').style.display = 'block';
-    document.getElementById('first-screen').style.display = 'none';
-    //obtener name, email y password
-    //const nameForSignUp = name.value;
-    const email= emailTxt.value;
-    const password = passwordTxt.value;
-    const auth = firebase.auth();
-    //sign Up
-    auth.createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-      });
+  e.preventDefault();
+  // obtener name, email y password
+  // const name = nameTxt.value;
+  const email = emailTxt.value;
+  const password = passwordTxt.value;
+  const auth = firebase.auth();
+  // sign Up
+  auth.createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    if (errorCode === 'auth/weak-password') {
+      errorWeakPassword.innerHTML = 'Tu contraseña debe tener 6 carácteres :)';
+    } else if (errorCode === 'auth/invalid-email') {
+      errorInvalidEmail.innerHTML = '¡Hey! Ingresa un correo electronico válido';
+    } else {
+      hideFirstScreenAndWelcome();
+      showWelcomeScreen();
+    }
+  });
 });
 
+// Añadir evento Crear usuario con Google
+btnSignUpWithGoogle.addEventListener('click', e => {
+  e.preventDefault();
+  console.log('entro'); 
+  // obtener name, email y password
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().languageCode = 'es';
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    let token = result.credential.accessToken;
+    // The signed-in user info.
+    let user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    // The email of the user's account used.
+    let email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    let credential = error.credential;
+    // ...
+  });
+  hideFirstScreenAndWelcome();
+  showWelcomeScreen();
+});
+
+// Función muestra segunda pantalla
+const showSecondScreen = () => {
+  document.getElementById('first-screen').style.display = 'none';
+  document.getElementById('second-screen').style.display = 'block';
+};
+// Función esconde segunda y tercera pantalla
+const hideFirstScreenAndWelcome = () => {
+  document.getElementById('first-screen').style.display = 'none';
+  document.getElementById('second-screen').style.display = 'none';
+};
+// Función crea template literal para bienvenida una vez el usuario esta registrado
+const showWelcomeScreen = () => {
+  const newUserWelcome = 
+        `<h1>Bienvenido</h1>
+        <p>Eres nueva usuaria</p>
+        `;
+  window.tempNewUserWelcome += newUserWelcome;
+  welcome.innerHTML = window.tempNewUserWelcome;
+};
 import { myFunction } from './lib/index.js';
 
 myFunction();
