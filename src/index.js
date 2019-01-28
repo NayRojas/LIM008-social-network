8e377458c0bed18c5a03077108f64036b62f603b
melanyvlm <mel.valencia28@outlook.com>
2019-01-28 12:15:25 -0500

    Log in with facebook intento 1

--- src/index.js
+++ src/index.js
@@ -33,6 +33,42 @@ btnCreateUser.addEventListener('click', e => {
   });
 });
 
+// Autenticación con Facebook
+
+const faceProvider = new firebase.auth.FacebookAuthProvider();
+
+// Log In variables
+const btnFacebook = document.getElementById('btn-facebook');
+
+
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+
+// Redireccionamiento
+firebase.auth().signInWithRedirect(provider);
+
+
+// Add Input event
+
+
+
+
+
 // import { myFunction } from './lib/index.js';
 
 // myFunction();
